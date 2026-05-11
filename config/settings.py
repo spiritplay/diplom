1d911eac8301f7a97312f284b81aabf670742178
egors <egorsmaga5@gmail.com>
2026-05-11 14:17:14 +0300

    diplom (3 commit)

--- config/settings.py
+++ config/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
+    BASE_DIR / 'static',
 ]
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR / 'media'
